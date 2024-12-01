c940ee8689bb3e38a19ef48c5dc6766b4258a214
DiegoGuerra00 <di.guerra00@gmail.com>
2024-12-01 19:55:29 -0300

    update window title and size

--- pendulum/main.cpp
+++ pendulum/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char **argv) {
   try {
     abcg::Application app(argc, argv);
     Window window;
+    window.setWindowSettings({.width = 800, .height = 600, .title = "Pêndulo"});
     app.run(window);
   } catch (std::exception const &e) {
     fmt::print("Exception: {}\n", e.what());
